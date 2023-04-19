ac5a47210f46f0a8fdc48a6fd4aa06d91894d785
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-04-19 21:47:14 +0400

    feat - add route on logo and fix reset mail

--- resources/views/components/verify/layout.blade.php
+++ resources/views/components/verify/layout.blade.php
@@ -1,7 +1,9 @@
 <x-layout>
     <div class="container mx-auto flex flex-col items-center">
         <header class="mt-20">
-            <img src="{{ asset('assets/images/logo.svg') }}" alt="Logo" class="md:w-44 md:h-12">
+            <a href="{{ route('home.index') }}">
+                <img src="{{ asset('assets/images/logo.svg') }}" alt="Logo" class="md:w-44 md:h-12">
+            </a>
         </header>
         {{ $slot }}
     </div>
--- resources/views/email/reset-password.blade.php
+++ resources/views/email/reset-password.blade.php
@@ -2,8 +2,8 @@
     style="display: flex; padding: 1rem; flex-direction: column; align-items: center; margin: 0 auto; height: 100vh; justify-content: center;">
     <img src="https://i.ibb.co/MVCCTpK/email.png" alt="mainmail" style="border: 0;">
     <p style="font-weight: 900; font-size: 1.5rem; color: #2D3748; margin-top: 1.5rem;">Recover password</p>
-    <p style="font-size: 1.125rem; margin-top: 1rem;">click this button to recover a password</p>
+    <p style="font-size: 1.125rem; margin-top: 1rem; max-width: 100%">click this button to recover a password</p>
     <a href="{{ $url }}"
-        style="background-color: #34D399; color: #fff; font-weight: 900; border-radius: 0.5rem; padding: 0.75rem 4.2rem; margin-top: 1rem; text-decoration: none; display: inline-block;">RECOVER
+        style="background-color: #34D399; color: #fff; font-weight: 900; border-radius: 0.5rem; padding: 0.75rem 3rem; margin-top: 1rem; text-decoration: none; display: inline-block; max-width: 100%">RECOVER
         PASSWORD</a>
 </div>
