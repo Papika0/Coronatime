a6f111dff42cd1fcebf32e4688c001358bf49144
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-04-24 17:43:13 +0400

    feat - install translatable/ add route, controller, middleware/model

--- app/Models/Countries.php
+++ app/Models/Countries.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Translatable\HasTranslations;
 
 class Countries extends Model
 {
-	use HasFactory;
+	use HasFactory, HasTranslations;
 
 	protected $guarded = [];
+
+	public $translatable = ['name'];
 
 	public function stats()
 	{
