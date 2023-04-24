a341c3de8984378c03e63e1d431bfa6409b24b53
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-04-24 17:43:47 +0400

    feat - add lang folder for en/ka

---
+++ lang/en/dashboard.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'Worldwide Statistics'  => 'Worldwide Statistics',
+	'Statistics by country' => 'Statistics by country',
+	'worldwide'             => 'Worldwide',
+	'by country'            => 'By Country',
+	'New Cases'             => 'New cases',
+	'Recovered'             => 'Recovered',
+	'Death'                 => 'Death',
+	'search'                => 'Search by country',
+	'Location'              => 'Location',
+	'georgian'              => 'Georgian',
+	'english'               => 'English',
+	'log out'               => 'Log Out',
+	'Deaths' 			            => 'Deaths',
+];
