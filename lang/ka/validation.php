c006439428a8816e571b924079e8d622c527d9a7
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-04-24 19:48:36 +0400

    feat - add static lang keys and values

--- lang/ka/validation.php
+++ lang/ka/validation.php
@@ -1,4 +1,22 @@
 <?php
 
 return [
+	'required' => 'აუცილებელია შეივსოს :attribute.',
+
+	'min' => [
+		'string' => ':attribute უნდა იყოს მინიმუმ :min ასო.',
+	],
+
+	'email'    => ':attribute უნდა იყოს ვალიდური.',
+
+	'exists'   => 'არჩეული :attribute არასწორია.',
+	'unique'   => 'ასეთი :attribute უკვე არსებობს.',
+
+	'same'       => ':attribute და :other უნდა ემთხვეოდეს ერთმანეთს.',
+	'attributes' => [
+		'email'                 => 'ელ-ფოსტა',
+		'password'              => 'პაროლი',
+		'username'              => 'სახელი',
+		'password_confirmation' => 'პაროლი განმეორებით',
+	],
 ];
