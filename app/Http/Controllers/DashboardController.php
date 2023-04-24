f3f64b708ae7bfc1ef3d7c3b6ddd8edc644ab34c
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-04-24 14:05:19 +0400

    feat - add controller for dashboard and install scrollbar

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,17 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\View\View;
 
 class DashboardController extends Controller
 {
 	public function show(): View
 	{
-		return view('dashboard.worldwide');
+		$stats = DB::table('country_stats')
+		->selectRaw('sum(confirmed) as confirmed, sum(recovered) as recovered, sum(deaths) as deaths')
+		->first();
+		return view('dashboard.worldwide', ['stats' => $stats]);
 	}
 
-	public function showCountries(): View
+	public function showCountries(Request $request): View
 	{
-		return view('dashboard.countries');
+		$search = $request->search;
+		$sort = $request->sort;
+
+		$countryStats = DB::table('country_stats')
+			->select('country', 'confirmed', 'deaths', 'recovered')
+			->where('country', 'LIKE', "%$search%")
+			->when($sort, function ($query, $sort) {
+				[$column, $direction] = explode('_', $sort);
+				return $query->orderBy($column, $direction);
+			})
+			->get();
+
+		return view('dashboard.countries', ['countryStats' => $countryStats]);
 	}
 }
